5544541546a1b64be50bc5608884037ca5d03ce6
Lilli Schmidt <lilli.schmidt@fu-berlin.de>
2020-03-25 08:44:07 +0100

    some arrangement in the Constants file

--- src/latexDiffCrawler/Constants.java
+++ src/latexDiffCrawler/Constants.java
@@ -1,9 +1,11 @@
 package latexDiffCrawler;
 
 public class Constants {
-	public static String fileWithCtanMirrors = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\CtanMirrorsBig4.txt";
-	public static String MIRROR_DIR = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\repo\\mirrors";
-	public static String MASTER_DIR = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\repo\\master";
+	public static String ROOT_DIR = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\";
+	public static String fileWithCtanMirrors = ROOT_DIR + "CtanMirrorsBig4.txt";
+	public static String MIRROR_DIR = ROOT_DIR + "repo\\mirrors";
+	public static String MASTER_DIR = ROOT_DIR + "repo\\master";
 	public static String DANTE = "http://dante.ctan.org/tex-archive/";
-	public static String MASTER_HASHES = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\masterHashes.txt";
+	public static String MASTER_HASHES = ROOT_DIR + "masterHashes.txt";
+	public static String RSCYNC_LOG = ROOT_DIR + "rysnc.log";
 }
