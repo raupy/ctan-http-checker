29dda01161903cbda53a5d9912157edff8121897
Lilli Schmidt <lilli.schmidt@fu-berlin.de>
2020-04-07 12:00:35 +0200

    brings order to Constants file

--- src/latexDiffCrawler/Constants.java
+++ src/latexDiffCrawler/Constants.java
@@ -1,14 +1,18 @@
 package latexDiffCrawler;
 
 public class Constants {
-	public static String ROOT_DIR = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\";
-	public static String fileWithCtanMirrors = ROOT_DIR + "CtanMirrorsBig4.txt";
+	public static String REPO_DIR = "C:\\Users\\Lilli\\eclipse-workspace\\LaTexCrawler\\repo\\";
+	public static String MIRROR_DIR = REPO_DIR + "mirrors";
+	public static String MASTER_DIR = REPO_DIR + "master";
+	public static String MASTER_DIFFICULT_FILES_DIR = REPO_DIR + "masterDifficultFiles";
+	public static String RSCYNC_LOG = REPO_DIR + "rsync.log";
+	
+	public static String ROOT_DIR = "C:\\Users\\Lilli\\git\\ctan_checker_http\\src\\";
 	public static String mirrorData = ROOT_DIR + "mirrorData.txt";
-	public static String MIRROR_DIR = ROOT_DIR + "repo\\mirrors";
-	public static String MASTER_DIR = ROOT_DIR + "repo\\master";
-	public static String MASTER_DIFFICULT_FILES_DIR = ROOT_DIR + "repo\\masterDifficultFiles";
 	public static String DIFFICULT_FILES = ROOT_DIR + "difficultFiles.txt";
-	public static String DANTE = "http://dante.ctan.org/tex-archive/";
 	public static String MASTER_HASHES = ROOT_DIR + "masterHashes.txt";
-	public static String RSCYNC_LOG = ROOT_DIR + "rsync.log";
+	public static String fileWithCtanMirrors = ROOT_DIR + "CtanMirrors.txt";
+
+	public static String DANTE = "http://dante.ctan.org/tex-archive/";
+	
 }
